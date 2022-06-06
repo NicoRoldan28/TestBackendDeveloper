8d790b4693aca7b3f3e3d71a466a5bdce3fa3504
nicolas roldan <nroldan@mobydigital.com>
2022-06-06 11:42:07 -0300

    Se crearon los controllers del candidato y de la tecnologia por defecto

---
+++ src/main/java/com/example/testbackenddeveloper/controller/TechnologyController.java
@@ -0,0 +1,39 @@
+package com.example.testbackenddeveloper.controller;
+
+import com.example.testbackenddeveloper.models.entities.Technology;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping(value = "/api/techonology")
+@RestController
+public class TechnologyController {
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@RequestBody Technology technology) {
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Object>>getAll() {
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object>getById(@RequestParam Long id) {
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object>deleteById(@RequestParam Long id) {
+    }
+
+    @PatchMapping("/{id")
+    public ResponseEntity<Object>updateCandidate(@RequestParam Long id) {
+    }
+}
